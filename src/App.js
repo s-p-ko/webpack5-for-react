10912d1e1063f425946d9a782404c19b7d6f4964
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-08-02 17:27:36 +0300

    Add src folder & update package.json

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function App() {
+    return (<div>
+        <h1>Welcome to React App</h1>
+        <p>Where learning Webpack 5</p>
+    </div>)
+}
+
+export default App
